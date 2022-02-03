ead54709c46cece3df978a289f23bc84a03eac39
Pol Vilarrasa <pol.vilarrasa@estudiants.urv.cat>
2022-02-03 21:10:14 +0100

    dev

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Calculator from './Components/Calculator'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
